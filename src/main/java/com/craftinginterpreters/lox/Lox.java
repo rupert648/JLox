f9159774162e7967e02229c68d6ef5cd08db3a8f
rupert648 <rupertcarr@hotmail.co.uk>
2022-12-12 21:57:54 +0000

    something

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -52,7 +52,12 @@ public class Lox {
         Parser parser = new Parser(tokens);
         List<Stmt> statements = parser.parse();
 
+        if (hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        
+        // stop if there was a resolution error
         if (hadError) return;
 
         interpreter.interpret(statements);
